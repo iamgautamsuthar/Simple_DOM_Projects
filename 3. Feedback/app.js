8b7fba2bc197704555da537fdf5f188908b7d529
Gautam Suthar <iamgautamsuthar@gmail.com>
2023-12-11 07:21:07 +0530

    Project Cancel

---
+++ 3. Feedback/app.js
@@ -0,0 +1,9 @@
+const ratings = document.getElementsByClassName("ratings");
+const ratingsContainer = document.getElementsByClassName("ratings-container");
+const sendBtn = document.getElementById("send");
+const panel = document.getElementById("panel");
+
+
+let selectedRating = "Satisfied";
+
+
